e71f2278fe8acb16f8553734705f8e5fc28e95e9
virajhmehta <virajmehta2499@gmail.com>
2024-02-09 19:42:41 +0530

    Postman and optional coverImage Code

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -31,15 +31,14 @@ const registerUser  = asyncHandler(async (req, res) => {
     // that we can write using some method
 
     // like this we can write many validation, exa: for email is having @ or not
-    if(
-        [fullName, email, username, password].some((field) => field?.trim() === ""
-        )
-    ){
+    if (
+        [fullName, email, username, password].some((field) => field?.trim() === "")
+    ) {
         throw new ApiError(400, "All fields are required")
     }
 
     // user exists  from db
-    const existedUser = User.findOne({
+    const existedUser = await  User.findOne({
         $or: [{username}, {email}]
     })
 
@@ -47,31 +46,37 @@ const registerUser  = asyncHandler(async (req, res) => {
         throw new ApiError(409, "User with email or username exist");
     }
 
+    console.log(req.files);
     // check for images, avatar
-    const avatarLocalPath = req.files?.avtar[0]?.path;
-    const coverImageLocalPath = req.files?.coverImage[0]?.path;
+    let avatarLocalPath = req.files?.avatar[0]?.path;
+    // let coverImageLocalPath = req.files?.coverImage[0]?.path;
+
+    let coverImageLocalPath;
+    if(req.files && Array.isArray(req.files.coverImage) && req.files.coverImage.length > 0){
+        coverImageLocalPath = req.files.coverImage[0].path;
+    } // if we file and proper array or not 
 
     if (!avatarLocalPath) {
-        throw new ApiError(400, "Avtar File is required")
+        throw new ApiError(400, "Avatar path File is required")
     }
 
     // upload to the cloudinary
     // it will give localFilePath that pass in defined cloudinary method
 
-    const avatar = await uploadOnCloudinary(avatarLocalPath)
-    const coverImage = await uploadOnCloudinary(coverImageLocalPath)
+    let avatar = await uploadOnCloudinary(avatarLocalPath)
+    let coverImage = await uploadOnCloudinary(coverImageLocalPath)
 
     // checking avatar
 
     if (!avatar) {
-        throw new ApiError(400, "Avtar File is required")
+        throw new ApiError(400, "Avatar file is required")
     }
 
 
     // db entry
     const user = await User.create({
         fullName,
-        avatar: avtar.url, // storing url in db
+        avatar: avatar.url, // storing url in db
         coverImage: coverImage?.url || "", // checking here since it is optional not like avatar
         email,
         password,
@@ -79,12 +84,12 @@ const registerUser  = asyncHandler(async (req, res) => {
     })
 
     // remove passs and refresh token
-    const createdUSer = await User.findById(user._id).select(
-        "-password -refresToken"
+    const createdUser = await User.findById(user._id).select(
+        "-password -refreshToken"
     )
     // using select method we write that we don't want
     
-    if(!createdUSer)
+    if(!createdUser)
         throw  new ApiError(500, "Something went wrong while registring")
 
     // above is for if found issue while creating user
@@ -93,7 +98,7 @@ const registerUser  = asyncHandler(async (req, res) => {
     // Now crafting response
 
     return res.status(201).json( // created new obj and passing method acc to defined method
-        new ApiResponse(200,  createdUSer, "User registered Succesfully")
+        new ApiResponse(200,  createdUser, "User registered Succesfully")
     )
 
 
--- src/models/user.model.js
+++ src/models/user.model.js
@@ -25,7 +25,7 @@ const userSchema = new Schema({
         index: true
     },
     avatar: {
-        type: String, // cloudinary service url
+        type: String, // cloudinary url
         required: true,
     },
     coverImage: {
